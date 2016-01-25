34d7dbbf2b6b75eef9496453d646e7a959fb2736
Vladimir Panteleev <git@thecybershadow.net>
2016-01-25 23:26:31 +0000

    lib: Avoid using RTLD_NEXT

--- lib.c
+++ lib.c
@@ -14,9 +14,18 @@ xcb_randr_get_crtc_info_reply_t *
 xcb_randr_get_crtc_info_reply (xcb_connection_t                  *c  /**< */,
                                xcb_randr_get_crtc_info_cookie_t   cookie  /**< */,
                                xcb_generic_error_t              **e  /**< */) {
+	static void* handle = NULL;
+	if (!handle) {
+		handle = dlopen("/usr/lib/libxcb-randr.so.0", RTLD_LAZY);
+	}
+	if (!handle) {
+		fprintf(stderr, "4khack: Ack, dlopen failed!\n");
+		return NULL;
+	}
+
 	xcb_randr_get_crtc_info_reply_t* (*original_fun)(xcb_connection_t *c, xcb_randr_get_crtc_info_cookie_t   cookie, xcb_generic_error_t **e);
 
-	original_fun = dlsym(RTLD_NEXT, "xcb_randr_get_crtc_info_reply");
+	original_fun = dlsym(/*RTLD_NEXT*/handle, "xcb_randr_get_crtc_info_reply");
 	if (!original_fun) {
 		fprintf(stderr, "4khack: Ack, dlsym failed!\n");
 		return NULL;
